06a11e43d0bf63d97c834df1c944bcb6bc5dbb88
VaishaliBansal01 <vaishali.bansal01@gmail.com>
2023-04-04 15:40:02 +0530

    GitDemo

---
+++ GitDemo/src/test/java/com/gitDemo/GitDemo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitDemo.GitDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
